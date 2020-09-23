638713e3919235830805baf50e6bfade29ba9f4d
joilson <joilson_rg@hotmail.com>
2020-09-23 17:57:20 -0300

    feature: adiciona rota para retornar livros

--- alura/node.js-parte-1/server.js
+++ alura/node.js-parte-1/server.js
@@ -1,3 +1,3 @@
 const app = require('./src/config/custom-express');
 
-app.listen(3000, () => console.log('Server is running...'));
+app.listen(3000);
--- alura/node.js-parte-1/src/app/routes/routes.js
+++ alura/node.js-parte-1/src/app/routes/routes.js
@@ -2,4 +2,8 @@ module.exports = (app) => {
     app.get('/', (req, res) => {
         return res.json({ message: 'ok' });
     });
+
+    app.get('/books', (req, res) => {
+        return res.marko(require('../views/books/list/list.marko'));
+    });
 };
