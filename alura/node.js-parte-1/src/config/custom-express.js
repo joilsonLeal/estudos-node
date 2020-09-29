6af2ba8c44d7b0a2102b5590d628b072e16dd02d
joilson <joilson_rg@hotmail.com>
2020-09-29 08:57:55 -0300

    feature: adiciona middlewares e páginas para trabamento de erros (404 e 500)

--- alura/node.js-parte-1/src/config/custom-express.js
+++ alura/node.js-parte-1/src/config/custom-express.js
@@ -11,9 +11,9 @@ app.use('/estatico', express.static('src/app/public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 app.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      // look in urlencoded POST bodies and delete it
       var method = req.body._method;
       delete req.body._method;
       return method;
@@ -22,5 +22,17 @@ app.use(methodOverride(function (req, res) {
 
 const rotas = require('../app/rotas/rotas');
 rotas(app);
+
+app.use((req, res, next) => {
+  return res.status(404).marko(
+    require('../app/views/base/erros/404.marko')
+  );
+});
+
+app.use((erro, req, res, next) => {
+  return res.status(500).marko(
+    require('../app/views/base/erros/500.marko')
+  );
+});
 
 module.exports = app;
