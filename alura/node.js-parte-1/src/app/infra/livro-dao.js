859dcdd27aa29b358909989c2c110e925a599349
joilson <joilson_rg@hotmail.com>
2020-09-29 08:48:22 -0300

    chore: importação de arquivos da alura com novo layout

--- alura/node.js-parte-1/src/app/infra/livro-dao.js
+++ alura/node.js-parte-1/src/app/infra/livro-dao.js
@@ -1,46 +1,55 @@
 class LivroDao {
+
     constructor(db) {
         this._db = db;
     }
 
-    list() {
-        return new Promise((resolve, reject) =>
-            this._db.all(
-                'SELECT * FROM books',
-                (err, data) => {
-                    if(err)
-                        return reject(err);
-                    else
-                        return resolve(data);
-                }
-        ));
-    }
-
-    add(book) {
+    adiciona(livro) {
         return new Promise((resolve, reject) => {
             this._db.run(`
-                INSERT INTO books (
-                    title, 
-                    price, 
-                    description
-                ) VALUES (?, ?, ?)`,
-                [book.title, book.price, book.description],
-                err => {
-                    if(err)
-                        return reject('Não foi possível adicionar o livro.');
+                INSERT INTO livros (
+                    titulo, 
+                    preco,
+                    descricao
+                ) values (?,?,?)
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
+
                     resolve();
                 }
             )
         });
     }
 
-    selectById(id) {
+    lista() {
+        return new Promise((resolve, reject) => {
+            this._db.all(
+                'SELECT * FROM livros',
+                (erro, resultados) => {
+                    if (erro) return reject('Não foi possível listar os livros!');
+
+                    return resolve(resultados);
+                }
+            )
+        });
+    }
+
+    buscaPorId(id) {
 
         return new Promise((resolve, reject) => {
             this._db.get(
                 `
                     SELECT *
-                    FROM books
+                    FROM livros
                     WHERE id = ?
                 `,
                 [id],
@@ -54,20 +63,20 @@ class LivroDao {
         });
     }
 
-    update(book) {
+    atualiza(livro) {
         return new Promise((resolve, reject) => {
             this._db.run(`
-                UPDATE books SET
-                title = ?,
-                price = ?,
-                description = ?
+                UPDATE livros SET
+                titulo = ?,
+                preco = ?,
+                descricao = ?
                 WHERE id = ?
             `,
             [
-                book.title,
-                book.price,
-                book.description,
-                book.id
+                livro.titulo,
+                livro.preco,
+                livro.descricao,
+                livro.id
             ],
             erro => {
                 if (erro) {
@@ -79,13 +88,13 @@ class LivroDao {
         });
     }
 
-
     remove(id) {
+
         return new Promise((resolve, reject) => {
-            this._db.run(
+            this._db.get(
                 `
                     DELETE 
-                    FROM books
+                    FROM livros
                     WHERE id = ?
                 `,
                 [id],
--- alura/node.js-parte-1/src/config/database.js
+++ alura/node.js-parte-1/src/config/database.js
@@ -1,63 +1,64 @@
 const sqlite3 = require('sqlite3').verbose();
 const bd = new sqlite3.Database('data.db');
 
-const USERS_SCHEMA = `
-CREATE TABLE IF NOT EXISTS users (
+const USUARIOS_SCHEMA = `
+CREATE TABLE IF NOT EXISTS usuarios (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    full_name VARCHAR(40) NOT NULL UNIQUE, 
+    nome_completo VARCHAR(40) NOT NULL UNIQUE, 
     email VARCHAR(255) NOT NULL, 
-    password VARCHAR(255) NOT NULL
+    senha VARCHAR(255) NOT NULL
 )
 `;
 
-const INSERT_USER_1 = 
+const INSERIR_USUARIO_1 = 
 `
-INSERT INTO users (
-    full_name, 
+INSERT INTO usuarios (
+    nome_completo, 
     email,
-    password
-) SELECT 'Gabriel Leite', 'gabriel@alura.com.br', '123' WHERE NOT EXISTS (SELECT * FROM users WHERE email = 'gabriel@alura.com.br')
+    senha
+) SELECT 'Gabriel Leite', 'gabriel@alura.com.br', '123' WHERE NOT EXISTS (SELECT * FROM usuarios WHERE email = 'gabriel@alura.com.br')
 `;
 
-const BOOKS_SCHEMA = 
+const LIVROS_SCHEMA = 
 `
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS livros (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL, 
-    price REAL NOT NULL,
-    description TEXT DEFAULT ('') NOT NULL
+    titulo TEXT NOT NULL, 
+    preco REAL NOT NULL,
+    descricao TEXT DEFAULT ('') NOT NULL, 
+    url_capa TEXT
 )
 `;
 
-const INSERT_BOOK_1 = 
+const INSERIR_LIVRO_1 = 
 `
-INSERT INTO books (
-    title,
-    price,
-    description
-) SELECT 'Node na prática', 30.0, 'Como desenvolver com Node.' WHERE NOT EXISTS (SELECT * FROM books WHERE title = 'Node na prática')
+INSERT INTO livros (
+    titulo,
+    preco,
+    descricao
+) SELECT 'Node na prática', 30.0, 'Como desenvolver com Node.' WHERE NOT EXISTS (SELECT * FROM livros WHERE titulo = 'Node na prática')
 `;
 
-const INSERT_BOOK_2 = 
+const INSERIR_LIVRO_2 = 
 `
-INSERT INTO books (
-    title, 
-    price,
-    description
-) SELECT 'JavaScript na prática', 40.0, 'Como desenvolver com JavaScript.' WHERE NOT EXISTS (SELECT * FROM books WHERE title = 'JavaScript na prática')
+INSERT INTO livros (
+    titulo, 
+    preco,
+    descricao
+) SELECT 'JavaScript na prática', 40.0, 'Como desenvolver com JavaScript.' WHERE NOT EXISTS (SELECT * FROM livros WHERE titulo = 'JavaScript na prática')
 `;
 
 bd.serialize(() => {
     bd.run("PRAGMA foreign_keys=ON");
-    bd.run(USERS_SCHEMA);
-    bd.run(INSERT_USER_1);
-    bd.run(BOOKS_SCHEMA);
-    bd.run(INSERT_BOOK_1);
-    bd.run(INSERT_BOOK_2);
+    bd.run(USUARIOS_SCHEMA);
+    bd.run(INSERIR_USUARIO_1);
+    bd.run(LIVROS_SCHEMA);
+    bd.run(INSERIR_LIVRO_1);
+    bd.run(INSERIR_LIVRO_2);
 
-    bd.each("SELECT * FROM users", (err, user) => {
-        console.log('User: ');
-        console.log(user);
+    bd.each("SELECT * FROM usuarios", (err, usuario) => {
+        console.log('Usuario: ');
+        console.log(usuario);
     });
 });
 
