c327003fb02df1d395c0774e6927857f8ad14f8e
joilson <joilson_rg@hotmail.com>
2020-09-30 12:51:10 -0300

    chore: cria tabela de atendimentos

--- alura/agenda-petshop/index.js
+++ alura/agenda-petshop/index.js
@@ -1,10 +1,12 @@
 const customExpress = require('./config/customExpress');
 const conexao = require('./infra/connection');
+const Tables = require('./infra/tables');
 
 conexao.connect(erro => {
     if(erro)
         console.log(erro);
     else {
+        Tables.init(conexao);
         const app = customExpress();
         app.listen(3000, () => console.log('Server is running...')); 
     }
