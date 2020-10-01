9cb1420829fe4ff8c66da2867e9f0fb2af57b270
joilson <joilson.bica@somospi.com.br>
2020-10-01 16:22:52 -0300

    feature: adiciona tipagem nas classes

--- alura/alurabank/app/ts/controllers/NegociacaoController.ts
+++ alura/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,23 +1,24 @@
 class NegociacaoController {
-    private _inputData;
-    private _inputQuantidade;
-    private _inputValor;
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
 
     constructor() {
         const $ = document.querySelector.bind(document);
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
+        this._inputData = <HTMLInputElement>$('#data');
+        this._inputQuantidade = <HTMLInputElement>$('#quantidade');
+        this._inputValor = <HTMLInputElement>$('#valor');
     }
 
-    adiciona(event) {
+    adiciona(event: Event) {
 
         event.preventDefault();
 
         const negociacao = new Negociacao(
-            this._inputData.value,
-            this._inputQuantidade.value,
-            this._inputValor.value);
+            new Date(this._inputData.value.replace(/-/g, ',')),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
 
         console.log(negociacao);
     }
