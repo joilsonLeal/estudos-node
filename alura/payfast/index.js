f36de272d5d450a1223380f96cbb68640d15d03c
joilson <joilson_rg@hotmail.com>
2020-10-02 16:19:35 -0300

    chore: remoção de arquivos do projeto payfast

--- alura/payfast/index.js
+++
@@ -1,9 +0,0 @@
-const custonExpress = require('./config/custon-express');
-const tables = require('./infra/Tables');
-const db = require('./infra/connectionFactory');
-
-tables.init(db);
-
-const app = custonExpress();
-
-app.listen(3000);
