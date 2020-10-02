13e9399f7a7924e5d2a79891b7628122df522f4b
joilson <joilson_rg@hotmail.com>
2020-10-02 09:13:55 -0300

    chore: otimiza classe View

--- alura/alurabank/app/ts/views/View.ts
+++ alura/alurabank/app/ts/views/View.ts
@@ -1,4 +1,4 @@
-class View<T> {
+abstract class View<T> {
 
     protected _element: Element;
 
@@ -10,7 +10,5 @@ class View<T> {
         this._element.innerHTML = this.template(model);
     }
 
-    template(model: T): string{
-        throw new Error('Você deve implementar o método template.');
-    }
+    abstract template(model: T): string;
 }
